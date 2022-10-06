0eb98446bc926b2b4772a9e5b8cd0d7405c1bd7f
sms445566@naver.com <dongheehan@dongheeHanui-MacBookPro.local>
2022-10-06 23:44:47 +0900

    비밀번호 변경 테스트4

--- src/main/java/com/hotel/DelunaApplication.java
+++ src/main/java/com/hotel/DelunaApplication.java
@@ -1,15 +1,15 @@
 package com.hotel;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableScheduling
 @SpringBootApplication
-public class DelunaApplication implements WebMvcConfigurer{
+public class DelunaApplication implements WebMvcConfigurer {
 
 	static {
 		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
@@ -17,6 +17,17 @@ public class DelunaApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DelunaApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://52.8.235.242:3000");
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
